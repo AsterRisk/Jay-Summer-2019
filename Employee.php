0dea012d42b3eaed8712e18169a09a0021dc958f
AsterRisk <31030090+AsterRisk@users.noreply.github.com>
2019-05-28 00:29:08 -0500

    Add files via upload

---
+++ Employee.php
@@ -0,0 +1,19 @@
+<?php
+	public class Employee
+	{
+		Employee($nm, long $IDNo, $picFile, $QRC)
+		{
+			$this->name = $nm;
+			$this->ID = $IDNo;
+			$this->pic = $picFile;
+			$this->QR = $QRC;
+			$this->clocked = false;
+		}
+		
+		function clock() : String
+		{
+			$this->clocked = !($this->clocked);
+			return date("m/j/Y H:i");
+		}
+	}
+/?>
